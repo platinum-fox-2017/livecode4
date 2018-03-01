6af1b2be8be4d592432f49ee08338d654813f47e
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-01 09:25:29 +0700

    Challenge 4

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+// const router = require('./routes/index');
+const menus = require('./routes/menus');
 
 
-app.listen(300);
+app.set('views', __dirname+'/views/');
+app.set('view engine', 'ejs');
+
+//body parser
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+
+
+//routes
+// app.use('/', router);
+app.use('/menus',menus);
+
+
+
+const port = 3000;
+
+app.listen(port, function() {
+  console.log(`Server Starts on ${port}`);
+});
---
+++ routes/menus.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/:id/edit', (req, res) => {
+
+});
+
+router.post('/:id/edit', (req, res) => {
+
+});
+
+router.get('/:id/delete', (req, res) => {
+
+});
+module.exports = router;
