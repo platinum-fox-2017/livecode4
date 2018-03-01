b677232d92b44a0598314261acb2d4ad63f468ff
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-03-01 11:36:47 +0700

    Render back with input in menus

--- routes/menus.js
+++ routes/menus.js
@@ -2,12 +2,19 @@ const router = require('express').Router();
 const models = require('../models');
 
 router.get('/', (req, res) => {
+  let input = {
+    name: null,
+    menu_type: null,
+    rating: null,
+    price: null,
+    RestaurantId: null
+  }
   models.Menu.all({
     include: [{ model: models.Restaurant}],
     order: [['id','asc']]
   }).then((menus) => {
     models.Restaurant.all().then((restaurants) => {
-      res.render('menus/index',{menus: menus, restaurants: restaurants,err: null});
+      res.render('menus/index',{menus: menus, restaurants: restaurants,err: null,input: input});
     });
   });
 });
@@ -18,10 +25,11 @@ router.post('/', (req, res) => {
   }).catch((err) => {
     console.log(err);
     models.Menu.all({
-      include: [{ model: models.Restaurant}]
+      include: [{ model: models.Restaurant}],
+      order: [['id','asc']]
     }).then((menus) => {
       models.Restaurant.all().then((restaurants) => {
-        res.render('menus/index',{menus: menus, restaurants: restaurants,err: err.message});
+        res.render('menus/index',{menus: menus, restaurants: restaurants,err: err.message,input: req.body});
       });
     });
   });
