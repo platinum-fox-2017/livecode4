d2279649189e16b232b4112cec7a5435364d2698
Reza Pramudhika <rezapramudhika@gmail.com>
2018-03-01 11:25:27 +0700

    Challenge 8-9

--- models/menu.js
+++ models/menu.js
@@ -5,23 +5,44 @@ module.exports = (sequelize, DataTypes) => {
     menu_type: {
       type: DataTypes.STRING,
       validate: {
-        // isIn: {
-        //   arg: [['food', 'drink']],
-        //   msg: ""
-        // }
         isType(value){
           if(value !== 'food'){
             if(value !== 'drink'){
               throw new Error('Isi menu type dengan food/drink')
             }
           }
-        }
+        }        
       }
     },
     rating: DataTypes.INTEGER,
     price: DataTypes.INTEGER,
     RestaurantId: DataTypes.INTEGER
-  }, {});
+  }, {
+    hooks: {
+      beforeCreate: (menu, options) => {
+        if(menu.menu_type == 'food'){
+          if(menu.price == 0){
+            menu.price = 15000
+          }
+        }else if(menu.menu_type == 'drink'){
+          if(menu.price == 0){
+            menu.price = 10000
+          }
+        }
+      },
+      beforeUpdate: (menu, options) => {
+        if(menu.menu_type == 'food'){
+          if(menu.price == 0){
+            menu.price = 15000
+          }
+        }else if(menu.menu_type == 'drink'){
+          if(menu.price == 0){
+            menu.price = 10000
+          }
+        }
+      }
+    }
+  });
   Menu.associate = function(models) {
     Menu.belongsTo(models.Restaurant);
   };
